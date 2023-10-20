dd556e24f96772ac1dc08c05be15ca15926417a7
PepeFederico <pepe.00.fp@outlook.it>
2023-10-20 12:14:26 +0200

    SecondaProva SonarCloud

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        int a;
+        //prova cpdice sonarCloud
         System.out.println("Hello world!");
     }
 }
